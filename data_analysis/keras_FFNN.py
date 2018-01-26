ecbcb885c4bd81870f415c138251f8eee06380d7
Russ <russellkgw@gmail.com>
2018-01-26 08:35:16 +0200

    stuff

--- data_analysis/keras_FFNN.py
+++ data_analysis/keras_FFNN.py
@@ -24,7 +24,7 @@ exr_min, exr_max = 0.0, 0.0
 oil_min, oil_max = 0.0, 0.0
 fp_min, fp_max = 0.0, 0.0
 
-
+# Normilize the data
 for fp in fuel_data:
     exr_data = data_conn.exchange_rates(fp.date)
     for e in exr_data:
@@ -111,19 +111,60 @@ model.add(Dense(62, activation='relu'))
 model.add(Dense(31, activation='relu'))
 model.add(Dense(1, activation='linear'))
 # sgd = optimizers.SGD(lr=0.1)  # , decay=1e-6, momentum=0.9, nesterov=True
-model.compile(loss='mse', optimizer='sgd', metrics=['acc'])  # adagrad adam sgd rmsprop
+model.compile(loss='mse', optimizer='sgd')  # adagrad adam sgd rmsprop     , metrics=['acc']
 # print(model.summary())
+
+print('size of x: ' + str(len(x_array)))
 
 # import pdb; pdb.set_trace()
 # r = 1
 
-# y_array
+PRECISION = 6
 
-for _ in range(10):
-    for i in range(len(x_array)):
-        temp_x = np.array(x_array[i]).reshape(1, 124)
-        temp_y = np.array(y_array[i]).reshape(1, 1)
-        model.fit(temp_x, temp_y, verbose=1, epochs=1)
+# y_array
+### FIT ###
+print(' ### Fit the model ### ')
+x_fit = x_array[:240]
+y_fit = y_array[:240]
+EPOCHS = 10
+epoch_loss_list = []
+epoch_train_loss_avg = 0.0
+for e in range(EPOCHS):
+    epoch_loss = 0.0
+    for i in range(len(x_fit)):
+        temp_x = np.array(x_fit[i]).reshape(1, 124)
+        temp_y = np.array(y_fit[i]).reshape(1, 1)
+        res = model.fit(temp_x, temp_y, verbose=0, epochs=1)
+        epoch_loss += res.history['loss'][0]
+    
+    print('Training EPOCH: ' + str(e))
+    epoch_loss_list.append(epoch_loss/len(x_fit))
+    epoch_train_loss_avg += (epoch_loss/len(x_fit)) / EPOCHS
+
+print('AVERAGE TRAIN LOSS: ' + str(round(epoch_train_loss_avg, PRECISION)))
+# print('Epoch Losses:')
+# for e in range(len(epoch_loss_list)):
+#     print('EPOCH: ' + str(e + 1) + ' AVG LOSS: ' + str(round(epoch_loss_list[e], PRECISION)))
+
+
+### EVALUATE ###
+print(' ### Evaluate the model ### ')
+x_test = x_array[240:]
+y_test = y_array[240:]
+epoch_fit_loss_avg = 0.0
+for i in range(len(x_test)):
+    temp_x = np.array(x_test[i]).reshape(1, 124)
+    temp_y = np.array(y_test[i]).reshape(1, 1)
+    res = model.evaluate(temp_x, temp_y, verbose=0)
+    # print('EVAL: ' + str(i + 1) + ' LOSS: ' + str(res))
+    epoch_fit_loss_avg += res / len(x_test)
+
+print('AVERAGE FIT LOSS: ' + str(round(epoch_fit_loss_avg, PRECISION)))
+
+# r = model.evaluate(np.array(x_test[0]).reshape(1, 124), np.array(y_test[0]).reshape(1, 1), verbose=0)
+# print(str(r))
+
+# evaluate(self, x=None, y=None, batch_size=None, verbose=1, sample_weight=None, steps=None)
 
 
 # model.fit(np.array(x_array[0]).reshape(1, 124), np.array(y_array[0]).reshape(1, 1), verbose=2, epochs=1)
