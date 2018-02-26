9d8a5efef48e885208cd5cd0f18e7ffd11ba5395
Russ <russellkgw@gmail.com>
2018-02-26 08:49:13 +0200

    doing things

--- data_analysis/lin_fit.py
+++ data_analysis/lin_fit.py
@@ -5,18 +5,27 @@ import statsmodels.formula.api as smf
 from data_connector import DataConnector
 data_conn = DataConnector()
 
-X = [1, 2, 3, 4, 5]
-X = sm.add_constant(X)  # Add intercept component
-y = [2, 4, 6, 8, 10]
-
-# Fit regression model
-# results = sm.OLS(y, X).fit()
-
-# Inspect the results
-# print(results.summary())
-
 x_array, y_array = data_conn.fuel_prices_dates(start_date='2004-04-03', flatten=False, percentage_change=False, normilize=False)  # start_date='2004-04-03'    None is all
-
 x_array = [np.average(x[:,0] * x[:,1]) for x in x_array]  # rand value of oil price
 
-# import pdb; pdb.set_trace()
+SPLIT = 140
+x_array = sm.add_constant(x_array)
+x_fit = x_array[:SPLIT]
+y_fit = y_array[:SPLIT]
+
+results = sm.OLS(y_fit, x_fit).fit()
+print(results.summary())
+
+print('Test x values:')
+print(str(x_array[SPLIT:][:,1]))
+
+print('Test y values:')
+print(str(y_array[SPLIT:]))
+
+print('Predicted y values:')
+ans = results.predict(x_array[SPLIT:])
+print(str(ans))
+
+print('MSE:')
+mse = np.average((np.array(y_array[SPLIT:]) - np.array(ans)) * (np.array(y_array[SPLIT:]) - np.array(ans)))
+print(str(mse))
